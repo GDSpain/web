1ed888d8f062d427328e0ec84c28528301a90c45
GDSpain <155786276+GDSpain@users.noreply.github.com>
2024-07-23 22:40:39 +0200

    Update Script.js

--- StatsViewer/Script.js
+++ StatsViewer/Script.js
@@ -11,11 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
             })
             .then(data => {
                 elemento.innerHTML = data;
-                // Recuperar y aplicar la posición de desplazamiento después de cargar el nuevo contenido
-                const scrollPosition = localStorage.getItem('scrollPosition');
-                if (scrollPosition) {
-                    window.scrollTo(0, parseInt(scrollPosition, 10));
-                }
             })
             .catch(error => {
                 console.error('Hubo un problema con la solicitud Fetch:', error);
@@ -26,39 +21,6 @@ document.addEventListener("DOMContentLoaded", function() {
     if (contenedor) {
         cargarContenido('Contenido.html', contenedor);
     } else {
-        console.error('No se encontró el contenedor con el ID "Contenido".');
-    }
-
-    // Guardar la posición de desplazamiento antes de abandonar la página
-    window.addEventListener('beforeunload', function() {
-        localStorage.setItem('scrollPosition', window.scrollY);
-    });
-
-    // Añadir manejadores de eventos a los enlaces para cargar contenido dinámicamente
-    document.querySelectorAll('a').forEach(enlace => {
-        enlace.addEventListener('click', function(event) {
-            event.preventDefault();
-            const url = this.getAttribute('href');
-            if (contenedor) {
-                // Guardar la posición de desplazamiento antes de cargar el nuevo contenido
-                localStorage.setItem('scrollPosition', window.scrollY);
-                cargarContenido(url, contenedor);
-                // Actualizar la URL sin recargar la página
-                window.history.pushState({ path: url }, '', url);
-            }
-        });
-    });
-
-    // Manejar el evento popstate para la navegación del historial
-    window.addEventListener('popstate', function(event) {
-        if (event.state && event.state.path) {
-            cargarContenido(event.state.path, contenedor);
-        }
-    });
-
-    // Recuperar y aplicar la posición de desplazamiento al cargar la página
-    const scrollPosition = localStorage.getItem('scrollPosition');
-    if (scrollPosition) {
-        window.scrollTo(0, parseInt(scrollPosition, 10));
+        console.error('No se encontró el contenedor con el ID "contenido".');
     }
 });
